c8b67c5ce0faf87c175fb2a3e36746f2c7baa769
Gleicon Moraes <gleicon@gmail.com>
2011-08-20 00:35:39 -0300

    initial server sent events support

--- cyclone/web.py
+++ cyclone/web.py
@@ -1022,6 +1022,44 @@ class WebSocketHandler(RequestHandler):
         x = int(''.join(nums))/spaces
         return x
 
+class SSEHandler(RequestHandler):
+	def __init__(self, application, request):
+		RequestHandler.__init__(self, application, request)
+		self.transport = request.connection.transport
+		self._auto_finish = False
+
+	def sendEvent(self, message, event=None, eid=None, retry = None):
+		"""
+			sendEvent is the single method to send events to clients.
+			message: the event itself
+			event: optional event name
+			eid: optional event id to be used as Last-Event-ID header or e.lastEventId property
+			retry: set the retry timeout in ms. default 3 secs.
+		"""
+		if isinstance(message, dict):
+		    message = escape.json_encode(message)
+		if isinstance(message, unicode):
+		    message = message.encode("utf-8")
+		assert isinstance(message, str)
+
+		if eid: self.transport.write("id: %s\n" % eid)		
+		if event: self.transport.write("event: %s\n" % event)
+		if retry: self.transport.write("retry: %s\n" % retry)
+		self.transport.write("data: " + message + "\n\n")
+		
+	def _execute(self, transforms, *args, **kwargs):
+		log.msg('SSE new connection from %s' % self.request.remote_ip)
+		self.set_header("Content-Type", "text/event-stream")
+		self.set_header("Cache-Control", "no-cache")
+		self.set_header("Connection", "keep-alive")
+		self.flush()
+		self.request.connection.setRawMode()
+		self.bind()
+
+	def bind(self):
+		pass
+	
+		
 def asynchronous(method):
     """Wrap request handler methods with this if they are asynchronous.
 
