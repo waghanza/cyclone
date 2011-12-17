612b336e2e91f94310bd9e595868cf1334cb5144
Toby Padilla <tobypadilla@gmail.com>
2011-12-17 07:46:26 -0800

    more cleanup, start of draft 76 support

--- cyclone/websocket.py
+++ cyclone/websocket.py
@@ -37,6 +37,11 @@ class WebSocketProtocol(object):
     def sendMessage(self, message):
         pass
 
+    def loseConnection(self, message):
+        self.transport.write("HTTP/1.1 403 Forbidden\r\nContent-Length: " +
+            str(len(message)) + "\r\n\r\n" + message)
+        return self.transport.loseConnection()
+
 
 class WebSocketProtocol17(WebSocketProtocol):
     def __init__(self, handler):
@@ -60,10 +65,7 @@ class WebSocketProtocol17(WebSocketProtocol):
     def acceptConnection(self):
         versions = ('7', '8', '13')
         if self.request.headers['Sec-WebSocket-Version'] not in versions:
-            message = "Unsupported WebSocket Protocol Version"
-            self.transport.write("HTTP/1.1 403 Forbidden\r\nContent-Length: " +
-                str(len(message)) + "\r\n\r\n" + message)
-            return self.transport.loseConnection()
+            return self.loseConnection("Unsupported WebSocket Protocol Version")
 
         log.msg('Using ws spec (draft 17)')
         if 'Origin' in self.request.headers:
@@ -89,8 +91,7 @@ class WebSocketProtocol17(WebSocketProtocol):
         log.msg('raw data length %d' % self._raw_data_len)
 
         if self._partial_data:
-            self._partial_data += data
-            data = self._partial_data
+            data = self._partial_data + data
             self._partial_data = None
 
         self._processFrameHeader(data)
@@ -190,11 +191,53 @@ class WebSocketProtocol17(WebSocketProtocol):
 
 
 class WebSocketProtocol76(WebSocketProtocol):
+    def __init__(self, handler):
+        WebSocketProtocol.__init__(self, handler)
+
+        self._k1 = None
+        self._k2 = None
+
     def acceptConnection(self):
         log.msg('accept connection!')
+        if self.request.headers.has_key('Sec-Websocket-Key1') == False or \
+            self.request.headers.has_key('Sec-Websocket-Key2') == False:
+            log.msg('Using old ws spec (draft 75)')
+            self.transport.write(
+                "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
+                "Upgrade: WebSocket\r\n"
+                "Connection: Upgrade\r\n"
+                "Server: cyclone/"+__version__+"\r\n"
+                "WebSocket-Origin: " + self.request.headers["Origin"] + "\r\n"
+                "WebSocket-Location: ws://" + self.request.host +
+                self.request.path + "\r\n\r\n")
+        else:
+            log.msg('Using ws draft 76 header exchange')
+            self._k1 = self.request.headers["Sec-WebSocket-Key1"]
+            self._k2 = self.request.headers["Sec-WebSocket-Key2"]
 
     def rawDataReceived(self, data):
         log.msg('raw data!')
+
+    def sendMessage(self, message):
+        if isinstance(message, dict):
+            message = escape.json_encode(message)
+        if isinstance(message, unicode):
+            message = message.encode("utf-8")
+        assert isinstance(message, str)
+        self.transport.write("\x00" + message + "\xff")
+
+    def _calculate_token(self, k1, k2, k3):
+        token = struct.pack('>ii8s', self._filterella(k1), self._filterella(k2), k3)
+        return hashlib.md5(token).digest()
+
+    def _filterella(self, w):
+        nums = []
+        spaces = 0
+        for l in w:
+            if l.isdigit(): nums.append(l)
+            if l.isspace(): spaces = spaces + 1
+        x = int(''.join(nums))/spaces
+        return x
 
 
 class WebSocketHandler(cyclone.web.RequestHandler):
@@ -216,32 +259,6 @@ class WebSocketHandler(cyclone.web.RequestHandler):
 
     def sendMessage(self, message):
         self.ws_protocol.sendMessage(message)
-        """
-        if self._protocol == 10:
-            message = unicode(message, "utf-8")
-            length = len(message)
-            newFrame = []
-            newFrame.append(0x81)
-            newFrame = bytearray(newFrame)
-            if length <= 125:
-                newFrame.append(length)
-            elif length > 125 and length < 65536:
-                newFrame.append(126)
-                newFrame += struct.pack('!H', length)
-            elif length >= 65536:
-                newFrame.append(127)
-                newFrame += struct.pack('!Q', length)
-
-            newFrame += message.encode()
-            self.transport.write(str(newFrame))
-        else:
-            if isinstance(message, dict):
-                message = escape.json_encode(message)
-            if isinstance(message, unicode):
-                message = message.encode("utf-8")
-            assert isinstance(message, str)
-            self.transport.write("\x00" + message + "\xff")
-        """
 
     def _handle_request_exception(self, e):
         if isinstance(e, HTTPError):
@@ -268,77 +285,6 @@ class WebSocketHandler(cyclone.web.RequestHandler):
             self._postheader = False
             self.flush()
             return
-        elif ord(data[0]) & 0x80 == 0x80 and self._protocol == 10:
-            ## Draft 10 Reference:
-            ## https://github.com/oberstet/Autobahn/blob/master/lib/python/autobahn/websocket.py
-
-            buffered_len = len(data)
-            ## FIN, RSV, OPCODE
-            ##
-            b = ord(data[0])
-            frame_fin = (b & 0x80) != 0
-            frame_rsv = (b & 0x70) >> 4
-            frame_opcode = b & 0x0f
-
-            ## MASK, PAYLOAD LEN 1
-            ##
-            b = ord(data[1])
-            frame_masked = (b & 0x80) != 0
-            frame_payload_len1 = b & 0x7f
-
-            ## compute complete header length
-            ##
-            if frame_masked:
-                mask_len = 4
-            else:
-                mask_len = 0
-
-            if frame_payload_len1 <  126:
-                frame_header_len = 2 + mask_len
-            elif frame_payload_len1 == 126:
-                frame_header_len = 2 + 2 + mask_len
-            elif frame_payload_len1 == 127:
-                frame_header_len = 2 + 8 + mask_len
-
-            ## only proceed when we have enough data buffered for complete
-            ## frame header (which includes extended payload len + mask)
-            ##
-            if buffered_len >= frame_header_len:
-                i = 2
-                ## extract extended payload length
-                ##
-                if frame_payload_len1 == 126:
-                    frame_payload_len = struct.unpack("!H", data[i:i+2])[0]
-                    i += 2
-                elif frame_payload_len1 == 127:
-                    frame_payload_len = struct.unpack("!Q", data[i:i+8])[0]
-                    i += 8
-                else:
-                    frame_payload_len = frame_payload_len1
-
-                ## when payload is masked, extract frame mask
-                ##
-                frame_mask = None
-                frame_mask_array = []
-                if frame_masked:
-                    frame_mask = data[i:i+4]
-                    for j in range(0, 4):
-                        frame_mask_array.append(ord(frame_mask[j]))
-                    i += 4
-                    payload = bytearray(data[i:i+frame_payload_len])
-                    l = frame_payload_len
-                    for k in xrange(0, l):
-                        payload[k] ^= frame_mask_array[k % 4]
-
-                    self.messageReceived(str(payload))
-
-                    ## if there is still data after this frame, process again
-                    ##
-                    current_len = frame_header_len + frame_payload_len
-                    if current_len < buffered_len:
-                        self._rawDataReceived(data[current_len:])
-
-                    return
 
         try:
             idx = data.find("\xff")
@@ -355,6 +301,15 @@ class WebSocketHandler(cyclone.web.RequestHandler):
 
     def _execute(self, transforms, *args, **kwargs):
         self.request.connection.setRawMode()
+
+        try:
+            assert self.request.headers["Upgrade"].lower() == "websocket"
+            assert self.request.headers["Connection"].lower() == "upgrade"
+        except:
+            message = "Expected WebSocket Headers"
+            self.transport.write("HTTP/1.1 403 Forbidden\r\nContent-Length: " +
+                str(len(message)) + "\r\n\r\n" + message)
+            return self.transport.loseConnection()
 
         # The difference between version 8 and 13 is that in 8 the
         # client sends a "Sec-Websocket-Origin" header and in 13 it's
@@ -375,75 +330,13 @@ class WebSocketHandler(cyclone.web.RequestHandler):
         self.ws_protocol.acceptConnection()
 
         """
-        try:
-            assert self.request.headers["Upgrade"].lower() == "websocket"
-            assert self.request.headers["Connection"].lower() == "upgrade"
-        except:
-            message = "Expected WebSocket Headers"
-            self.transport.write("HTTP/1.1 403 Forbidden\r\nContent-Length: " +
-                str(len(message)) + "\r\n\r\n" + message)
-            return self.transport.loseConnection()
         else:
             try:
                 self.headersReceived()
             except Exception, e:
                 return self._handle_request_exception(e)
 
-            if 'Sec-Websocket-Version' in self.request.headers:
-                versions = ('7', '8', '13')
-                if self.request.headers['Sec-WebSocket-Version'] not in versions:
-                    message = "Unsupported WebSocket Protocol Version"
-                    self.transport.write("HTTP/1.1 403 Forbidden\r\nContent-Length: " +
-                        str(len(message)) + "\r\n\r\n" + message)
-                    return self.transport.loseConnection()
-
-                log.msg('Using ws spec (draft 10)')
-                if 'Origin' in self.request.headers:
-                    origin = self.request.headers['Origin']
-                else:
-                    origin = self.request.headers['Sec-Websocket-Origin']
-                key = self.request.headers['Sec-Websocket-Key']
-                accept = base64.b64encode(hashlib.sha1(key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11').digest())
-                self.transport.write(
-                    "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
-                    "Upgrade: WebSocket\r\n"
-                    "Connection: Upgrade\r\n"
-                    "Sec-WebSocket-Accept: " + accept + "\r\n"
-                    "Server: cyclone/" +__version__+ "\r\n"
-                    "WebSocket-Origin: " + origin + "\r\n"
-                    "WebSocket-Location: ws://" + self.request.host +
-                    self.request.path + "\r\n\r\n")
-                self._protocol = 10
-            elif self.request.headers.has_key('Sec-Websocket-Key1') == False or \
-                self.request.headers.has_key('Sec-Websocket-Key2') == False:
-                log.msg('Using old ws spec (draft 75)')
-                self.transport.write(
-                    "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
-                    "Upgrade: WebSocket\r\n"
-                    "Connection: Upgrade\r\n"
-                    "Server: cyclone/"+__version__+"\r\n"
-                    "WebSocket-Origin: " + self.request.headers["Origin"] + "\r\n"
-                    "WebSocket-Location: ws://" + self.request.host +
-                    self.request.path + "\r\n\r\n")
-                self._protocol = 75
-            else:
-                log.msg('Using ws draft 76 header exchange')
-                self.k1 = self.request.headers["Sec-WebSocket-Key1"]
-                self.k2 = self.request.headers["Sec-WebSocket-Key2"]
-                self._protocol = 76
         self._postheader = True
         self.connectionMade(*args, **kwargs)
         """
 
-    def _calculate_token(self, k1, k2, k3):
-        token = struct.pack('>ii8s', self._filterella(k1), self._filterella(k2), k3)
-        return hashlib.md5(token).digest()
-
-    def _filterella(self, w):
-        nums = []
-        spaces = 0
-        for l in w:
-            if l.isdigit(): nums.append(l)
-            if l.isspace(): spaces = spaces + 1
-        x = int(''.join(nums))/spaces
-        return x
