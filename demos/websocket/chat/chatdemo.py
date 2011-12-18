4457c3df2822fd6a60f70502af604274a5ddff14
Gleicon Moraes <gleicon@gmail.com>
2011-12-18 11:08:07 -0200

    more ws examples, new impl to be cleaned, may block due to protocol upgrade

---
+++ demos/websocket/chat/chatdemo.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+#
+# Copyright 2009 Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""Simplified chat demo for websockets.
+
+Authentication, error handling, etc are left as an exercise for the reader :)
+"""
+
+import cyclone.escape
+import cyclone.web
+import cyclone.websocket
+import os.path
+import uuid, sys
+from twisted.python import log
+from twisted.internet import reactor
+
+class Application(cyclone.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/chatsocket", ChatSocketHandler),
+        ]
+        settings = dict(
+            cookie_secret="43oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+            autoescape=None,
+        )
+        cyclone.web.Application.__init__(self, handlers, **settings)
+
+
+class MainHandler(cyclone.web.RequestHandler):
+    def get(self):
+        self.render("index.html", messages=ChatSocketHandler.cache)
+
+class ChatSocketHandler(cyclone.websocket.WebSocketHandler):
+    waiters = set()
+    cache = []
+    cache_size = 200
+
+    def open(self):
+        ChatSocketHandler.waiters.add(self)
+
+    def on_close(self):
+        ChatSocketHandler.waiters.remove(self)
+
+    @classmethod
+    def update_cache(cls, chat):
+        cls.cache.append(chat)
+        if len(cls.cache) > cls.cache_size:
+            cls.cache = cls.cache[-cls.cache_size:]
+
+    @classmethod
+    def send_updates(cls, chat):
+        log.msg("sending message to %d waiters", len(cls.waiters))
+        for waiter in cls.waiters:
+            try:
+                waiter.write_message(chat)
+            except:
+                log.err("Error sending message", exc_info=True)
+
+    def on_message(self, message):
+        log.msg("got message %r", message)
+        parsed = cyclone.escape.json_decode(message)
+        chat = {
+            "id": str(uuid.uuid4()),
+            "body": parsed["body"],
+            }
+        chat["html"] = self.render_string("message.html", message=chat)
+
+        ChatSocketHandler.update_cache(chat)
+        ChatSocketHandler.send_updates(chat)
+
+
+def main():
+    reactor.listenTCP(8888, Application())
+    reactor.run()
+
+
+if __name__ == "__main__":
+    log.startLogging(sys.stdout)
+    main()
