bd55ec3f1b25160290957274f743900e82b1490a
Alexandre Fiori <fiorix@gmail.com>
2012-02-10 11:02:07 -0500

    updated bottle demo

---
+++ demos/helloworld/helloworld_bottle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Copyright 2010 Alexandre Fiori
+# based on the original Tornado by Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import sys
+
+import cyclone.sqlite
+from cyclone.bottle import run, route
+
+@route("/")
+def index(web):
+    web.write('try <a href="/sqlite">/sqlite</a>\r\n')
+
+@route("/sqlite")
+def sqlite_get(web):
+    v = web.settings.sqlite.runQuery("select strftime('%Y-%m-%d')")
+    web.write("today is " + repr(v) + "\r\n")
+
+run(host="127.0.0.1", port=8888,
+    log=sys.stdout, # or any file descriptor
+    static_path="static", template_path="template",
+    sqlite=cyclone.sqlite.InlineSQLite(":memory:"))
+
