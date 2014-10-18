15da74b1091d61c1f20f4c87a5731ea56885f7ff
Jonathan Sandoval <jsandoval@utp.edu.co>
2014-10-17 22:04:23 -0500

    Changed body for postdata in facebook_request in FacebookGraphMixin.

--- cyclone/tests/test_auth.py
+++ cyclone/tests/test_auth.py
@@ -14,3 +14,37 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
+import urllib
+import cyclone.web
+
+from twisted.trial import unittest
+from twisted.internet import defer
+from cyclone.auth import FacebookGraphMixin
+from mock import patch, MagicMock
+
+
+class TestHandler(cyclone.web.RequestHandler,
+                  FacebookGraphMixin):
+    pass
+
+
+class TestFacebookGraphMixin(unittest.TestCase):
+    def setUp(self):
+        self.fgm = TestHandler(MagicMock(), MagicMock())
+
+    @patch('cyclone.auth.httpclient.fetch')
+    def test_facebook_request_post(self, mock):
+        _args = {'message': 'test message'}
+        self.fgm.facebook_request(
+            "/me/feed",
+            callback=self.fgm.async_callback(lambda x: x),
+            access_token='dummy_token',
+            post_args=_args
+        )
+
+        self.assertTrue(mock.called)
+        args, kwargs = mock.call_args
+        self.assertIn('postdata', kwargs)
+        self.assertEqual(kwargs['postdata'],
+                         urllib.urlencode(_args))
